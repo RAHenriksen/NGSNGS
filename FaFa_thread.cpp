68c09dfab6654524193b6e8cbbf459ed1d3291ff
RAHenriksen <rah@hotmail.dk>
2021-06-01 11:23:56 +0200

    while loop number of threads fafa

--- FaFa_thread.cpp
+++ FaFa_thread.cpp
@@ -44,7 +44,6 @@ struct Parsarg_for_Fafa_thread{
 
 //-------------- FUNCTIONS ----------------//
 
-// --------------------- //
 void* Fafa_thread_run(void *arg){
   Parsarg_for_Fafa_thread *struct_obj = (Parsarg_for_Fafa_thread*) arg;
 
@@ -74,7 +73,7 @@ void* Fafa_thread_run(void *arg){
     else{
       char nt[] = "tT";
       Deamin_char(seqmod,nt,readlength);
-      ksprintf(struct_obj->fqresult,">%s:%d-%d_length:%d\n%s\n",chr_name,start_pos,start_pos+readlength,readlength,seqmod);
+      ksprintf(struct_obj->fqresult,">%s:%d-%d_length:%d\n%s\n",chr_name,start_pos+1,start_pos+readlength,readlength,seqmod);
     }
 
     start_pos += readlength + 1;
@@ -87,53 +86,56 @@ void* Fafa_thread_run(void *arg){
   return NULL;
 }
 
-int main(int argc,char **argv){
+void* Create_threads(const char *fastafile,faidx_t *seq_ref,const char *output,int thread_no,int chr_total){
   //Loading in an creating my objects for the sequence files.
-  const char *fastafile = "/willerslev/users-shared/science-snm-willerslev-wql443/scratch/reference_files/Human/hg19canon.fa";
-  faidx_t *seq_ref = NULL;
-  seq_ref  = fai_load(fastafile);
-  assert(seq_ref!=NULL);
-  int chr_no = faidx_nseq(seq_ref);
+  
+  int chr_idx = 0;
+  int chr_no = 0;
+  
   
   //initialize mutex
   pthread_mutex_init(&data_mutex,NULL);
   
   //creating an array with the arguments to create multiple threads;
   //int nthreads=atai(argv[2]);
-  int nthreads = chr_no;
+  int nthreads = chr_total;
   pthread_t mythreads[nthreads];
   Parsarg_for_Fafa_thread struct_for_threads[nthreads];
 
-  //initialzie values that should be used for each thread
-  
-  for(int i=0;i<nthreads;i++){
-    struct_for_threads[i].chr_idx = i;
-    struct_for_threads[i].seq_ref = seq_ref;
-    struct_for_threads[i].fqresult=new kstring_t;
-    struct_for_threads[i].fqresult -> l = 0;
-    struct_for_threads[i].fqresult -> m = 0;
-    struct_for_threads[i].fqresult -> s = NULL;
-  }
+  int i = 0; int j = 0; int k = 0;
+  while(chr_idx < nthreads){
+    //initialzie values that should be used for each thread
+    for (i; i < std::min(chr_idx+thread_no,nthreads);i++){
+      struct_for_threads[i].chr_idx = i;
+      struct_for_threads[i].seq_ref = seq_ref;
+      struct_for_threads[i].fqresult=new kstring_t;
+      struct_for_threads[i].fqresult -> l = 0;
+      struct_for_threads[i].fqresult -> m = 0;
+      struct_for_threads[i].fqresult -> s = NULL;
+      std::cout << "chr_no" << i << std::endl;
+    }
 
-  //launch all worker threads
-  for(int i=0;i<nthreads;i++){
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
+    //launch worker threads
+    for (j; j < std::min(chr_idx+thread_no,nthreads);j++){
+      pthread_attr_t attr;
+      pthread_attr_init(&attr);
+      pthread_create(&mythreads[j],&attr,Fafa_thread_run,&struct_for_threads[j]);
+    }
 
-    pthread_create(&mythreads[i],&attr,Fafa_thread_run,&struct_for_threads[i]);
-  }
-    
-    //right now 22 thread are running at the same time
     //now join, this means waiting for all worker threads to finish
-  for(int i=0;i<nthreads;i++){
-    pthread_join(mythreads[i],NULL);
+    for(k;k<std::min(chr_idx+thread_no,nthreads);k++){
+      pthread_join(mythreads[k],NULL);
+    }
+
+    std::cout << "end of for loop" << std::endl;
+    chr_idx = chr_idx + thread_no;
   }
 
   pthread_mutex_destroy(&data_mutex);    
     //now all are done and we only write in main program.
     //now write everything
 
-  if (std::strcmp(argv[1], "gz") == 0){
+  if (std::strcmp(output, "gz") == 0){
     gzFile gz;
     gz = gzopen("threads_test.fa.gz","wb");
     for(int i=0;i<nthreads;i++){
@@ -141,7 +143,7 @@ int main(int argc,char **argv){
     }
     gzclose(gz);
   }
-  else if (std::strcmp(argv[1], "fa") == 0){
+  else if (std::strcmp(output, "fa") == 0){
     FILE *fp;
     fp = fopen("threads_test.fa","wb");
     for(int i=0;i<nthreads;i++){
@@ -149,21 +151,34 @@ int main(int argc,char **argv){
     }
     fclose(fp);
   }
+
+  return NULL;
 }
 
-/*
-  int chr_total = 9;//faidx_nseq(seq_ref);
-  int chr_idx = 0;
-  int chr_no = 0;
+// --------------------- //
+int main(int argc,char **argv){
+  const char *fastafile = "/willerslev/users-shared/science-snm-willerslev-wql443/scratch/reference_files/Human/hg19canon.fa";
+  faidx_t *seq_ref = NULL;
+  seq_ref  = fai_load(fastafile);
+  assert(seq_ref!=NULL);
+  int chr_total = faidx_nseq(seq_ref);
 
-  while(chr_idx < chr_total){
-    for (chr_no; chr_no < std::min(chr_idx+thread_no,chr_total);chr_no++){
-      threads -> push_back(std::thread(chrdata,seq_ref,chr_no,fp));
-      std::cout << "chromosome elements "<< chr_no << std::endl;
+  if(argc>2){
+    int thread_to_run = atoi(argv[2]);
+    if (thread_to_run > chr_total)
+    {
+      fprintf(stderr,"The number of threads (%i) cannot exceed the number of chromosomes (%i) in the reference file\n",thread_to_run,chr_total);
     }
-    std::cout << "end of for loop" << std::endl;
-    chr_idx = chr_idx + thread_no;
-  }*/
+    else
+    {
+      Create_threads(fastafile,seq_ref,argv[1],thread_to_run,chr_total);
+    }
+  }
+  else
+  {
+    Create_threads(fastafile,seq_ref,argv[1],chr_total,chr_total);
+  } 
+}
 
 // g++ SimulAncient_func.cpp FaFa_thread.cpp -std=c++11 -I /home/wql443/scratch/htslib/ /home/wql443/scratch/htslib/libhts.a -lpthread -lz -lbz2 -llzma -lcurl -Wall
-// ./a.out fa
+// ./a.out fa 5
