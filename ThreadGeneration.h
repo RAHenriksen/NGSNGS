cd6d0c53bce3dc705c5656485f85213eeb5fc0d4
wql443 <wql443@dandycomp03fl.unicph.domain>
2022-07-26 11:16:52 +0200

    Major clean up of thread generation

---
+++ ThreadGeneration.h
@@ -0,0 +1,13 @@
+#ifndef THREADGENERATION_H
+#define THREADGENERATION_H
+#define LENS 4096
+
+void* ThreadInitialization(faidx_t *seq_ref,int thread_no, int seed, size_t reads,const char* OutputName,const char* Adapt_flag,const char* Adapter_1,
+                        const char* Adapter_2,const char* OutputFormat,const char* SeqType,float BriggsParam[4],const char* Briggs_Flag,
+                        const char* Sizefile,int FixedSize,int SizeDistType,double val1,double val2,
+                        int qualstringoffset,const char* QualProfile1,const char* QualProfile2,int threadwriteno,
+                        const char* QualStringFlag,const char* Polynt,const char* ErrorFlag,const char* Specific_Chr[1024],const char* FastaFileName,
+                        const char* SubFlag,const char* SubProfile,int MisLength,int RandMacro,const char *VCFformat, char* Variant_flag,const char *VarType,
+                        char CommandArray[1024],const char* version,const char* HeaderIndiv,const char* NoAlign,size_t BufferLength);
+
+#endif
