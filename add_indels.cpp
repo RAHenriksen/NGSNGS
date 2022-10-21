1eef6074dce9edc54a6d28104f27a2e5a0050012
RAHenriksen <rah@hotmail.dk>
2022-10-21 16:56:05 +0200

    potential add_indels to fasta struct

--- add_indels.cpp
+++ add_indels.cpp
@@ -1,4 +1,5 @@
 #include "add_indels.h"
+#include "fasta_sampler.h"
 const char *bas = "ACGTN";
 const char *bas2 = "QWERY";
 
@@ -56,6 +57,144 @@ int add_indel(mrand_t *mr,char *frag,int readlength,double *pars){
   return ops[0]+ops[1];
 }
 
+
+/*while(beg<end-1){
+    //fprintf(stderr,"beg[%d]: %s len:%lu\n",beg,frag,strlen(frag));
+    //do dont anything if we are in the internal part of the fragmenth that is not getting sequenced
+    if(end>2*readlength && beg > readlength && ((end-beg)>readlength)){
+      beg++;
+      continue;
+    }
+    if(mrand_pop(mr)<pars[0]){
+      ops[0]++;
+      int len = Random_geometric_k(pars[2],mr)+1;
+      //fprintf(stderr,"Having insertion[%d]: %d end-1: %d\n",beg,len,end-1);
+      for(int i=end-1;i>=beg;i--){
+        //fprintf(stderr,"%d -> %d\n",i,i+len);
+        frag[i+len] = frag[i];
+      }
+      for(int i=0;i<len;i++){
+        frag[beg+i] = bas[mrand_pop_long(mr) %5]; //bas[mrand_pop_long(mr) %5];
+        //fprintf(stderr,"Setting insertion: %d\n",beg+i);
+      }
+      beg += len;
+      end += len;
+      continue;
+    }
+*/
+
+void add_indel_fs(fasta_sampler *fs,mrand_t *mr,double *pars){
+  for(int j=0;j<fs->nref;j++){
+    //fprintf(stderr,"ref %s \t chr number %d \n",fs->seqs_names[j],j);
+    int beg = 0;
+    int end = fs->seqs_l[j];
+    //fs->seqs[j]
+    //fprintf(stderr,"BEG %d \t END %d \n",beg,end);
+    int len_value = -1;
+    while(beg<end-1){
+      //fprintf(stderr,"beg[%d]: %s len:%lu\n",beg,frag,strlen(frag));
+      //do dont anything if we are in the internal part of the fragmenth that is not getting sequenced
+      if(end>2*150 && beg > 150 && ((end-beg)>150)){
+        beg++;
+        continue;
+      }
+      if(mrand_pop(mr)<pars[0]){
+        int len = Random_geometric_k(pars[2],mr)+1;
+        //fprintf(stderr,"Having insertion[%d]: %d end-1: %d\n",beg,len,end-1);
+        for(int i=end-1;i>=beg;i--){
+          //fprintf(stderr,"%d -> %d\n",i,i+len);
+          fs->seqs[j][i+len] = fs->seqs[j][i];
+        }
+        for(int i=0;i<len;i++){
+          fs->seqs[j][beg+i] = bas[mrand_pop_long(mr) %5]; //bas[mrand_pop_long(mr) %5];
+          //fprintf(stderr,"Setting insertion: %d\n",beg+i);
+        }
+        beg += len;
+        end += len;
+        continue;
+      }
+      if(mrand_pop(mr)<pars[1]){
+        int len = Random_geometric_k(pars[3],mr)+1;
+        //fprintf(stderr,"Having deletion[%d]: %d\n",beg,len);
+          //two cases: 1) is that deletion is in the middle of the fragment and we should shift all data to the left
+          //2) Deletion will cover the rest of the read. 
+          if(len+beg>end){
+            //case 2)
+            fs->seqs[j][beg+1] = '\0';
+            end = beg+1;
+            //should make debug statement to validate correctness of the thinking machines.
+          }else{
+            //case 1) we dont need to check for running of the data
+            for(int i=0;i<len;i++)
+              fs->seqs[j][beg+i] = fs->seqs[j][beg+i+len];
+            end -= len;
+            fs->seqs[j][end] = '\0';
+          }      
+        beg++;
+        continue;
+      }
+      //fprintf(stderr,"Nothing happens[%d]\n",beg);
+      beg++;
+    }
+  }
+}
+
+/*void add_indel_fs(fasta_sampler *fs,mrand_t *mr,double *pars){
+  fprintf(stderr,"ISNIDE ADD_INDELS \n");
+  fprintf(stderr,"indels Param 1 %f 2 %f 3 %f 4 %f\n",pars[0],pars[1],pars[2],pars[3]);
+  fprintf(stderr,"test %f\n",mrand_pop(mr));
+  fprintf(stderr,"test2 %d\n",abs(mrand_pop_long(mr)%5));
+  for(int j=0;j<fs->nref;j++){
+    int beg = 0;
+    int end = fs->seqs_l[j];//remember this is the length, not the index of the last element.
+    while(beg<end-1){
+      //fprintf(stderr,"beg[%d]: %lu len:%lu\n",beg,fs->seqs_l[j],strlen(fs->seqs[j]));
+      double dtemp = mrand_pop(mr);
+      fprintf(stderr,"dtemp %f\n",dtemp);
+      if(dtemp<pars[0]){
+        int len = Random_geometric_k(pars[2],mr)+1;
+        //fprintf(stderr,"Having insertion[%d]: %d end-1: %d\n",beg,len,end-1);
+        for(int i=end-1;i>=beg;i--){
+          //fprintf(stderr,"%d -> %d\n",i,i+len);
+          fs->seqs[j][i+len] = fs->seqs[j][i];
+        }
+        fprintf(stderr,"BEFORE BAS \n");
+        for(int i=0;i<len;i++){
+          fprintf(stderr,"test %d\n",i);
+          fs->seqs[j][beg] = bas[3];//bas[mrand_pop_long(mr) %5]; //bas[mrand_pop_long(mr) %5];
+          fprintf(stderr,"Setting insertion: %d\n",beg+i);
+        }
+        fprintf(stderr,"AFTER BAS \n");
+        beg += len;
+        end += len;
+        continue;
+      }
+      if(mrand_pop(mr)<pars[1]){
+        int len = Random_geometric_k(pars[3],mr)+1;
+        fprintf(stderr,"Having deletion[%d]: %d\n",beg,len);
+          //two cases: 1) is that deletion is in the middle of the fragment and we should shift all data to the left
+          //2) Deletion will cover the rest of the read. 
+          if(len+beg>end){
+            //case 2)
+            fs->seqs[j][beg+1] = '\0';
+            end = beg+1;
+            //should make debug statement to validate correctness of the thinking machines.
+          }
+          else{
+            //case 1) we dont need to check for running of the data
+            for(int i=0;i<len;i++)
+              fs->seqs[j][beg+i] = fs->seqs[j][beg+i+len];
+            end -= len;
+            fs->seqs[j][end] = '\0';
+          }      
+        beg++;
+        continue;
+      }
+      //fprintf(stderr,"Nothing happens[%d]\n",beg);
+      beg++;
+    }
+  }
+}*/
 
 #ifdef __WITH_MAIN__
 int main(int argc,char **argv){
--- add_indels.h
+++ add_indels.h
@@ -4,7 +4,10 @@
 #include <cstdio>//for stderr
 #include <cassert>//for assert
 #include "mrand.h"
+#include "fasta_sampler.h"
+
 int Random_geometric_k(const double p,mrand_t *mr);
 //pars is a general placeholder used for representing general parameters. Currently it is , insertion rate (prob), length(expontial lambda),deletion rate (prob), length(expontial lambda),
 int add_indel(mrand_t *mr,char *fragmentn,int readlength,double *pars);
+void add_indel_fs(fasta_sampler *fs,mrand_t *mr,double *pars);
 #endif
