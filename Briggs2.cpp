26f5013ae4fc1a0a01a0b3f7373ac4a75fdad579
RAHenriksen <rah@hotmail.dk>
2022-11-16 08:30:45 +0100

    Non_Biotin model counting Deamin

--- Briggs2.cpp
+++ Briggs2.cpp
@@ -17,7 +17,7 @@ extern char NtComp[5];
 extern const char *bass;
 
  
-int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, double delta, mrand_t *mr,char **res,int strandR1) {
+int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, double delta, mrand_t *mr,char **res,int strandR1,int& C_to_T_counter,int& G_to_A_counter,int& C_total,int& G_total) {
   int IsDeam = 0;
   assert(L<1024);
  
@@ -42,7 +42,6 @@ int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, 
   }
 
   //Please do a check in the following part, since it may shift by 1
- 
   
   strncpy(rasmus,ori,L);
   strncpy(thorfinn,ori,L); //Thorfinn equals Rasmus
@@ -95,10 +94,16 @@ int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, 
     fprintf(stderr,"i value %d\n",i);
   }*/
  
+
+  /*
+    5' CGTATACATAGGCACTATATCGACCACACT 3' RASMUS 
+    3'        TATCCGTGATATAGCTGGTGTGA 5' THORFINN
+  */
   for (int i = 0; i<l; i++){
     // left 5' overhangs, Thorfinn's DMG pattern is fully dependent on that of Rasmus.
 
     if (rasmus[i] == 'C' || rasmus[i] == 'c' ){
+      if (i == 0){C_total++;G_total++;}
       double u = mrand_pop(mr);
       if (u < delta_s){
         IsDeam = 1;
@@ -106,18 +111,25 @@ int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, 
         rasmus[i] = 'T'; 
         thorfinn[i] = 'A';
         //fprintf(stderr,"%d i %c %c\n",i,rasmus[i],thorfinn[i]);
+        if (i == 0){C_to_T_counter++;G_to_A_counter++;}
       }
     }
   }
-   
+  
+  /*
+    5' CGTATACATAGGCACTATATCGACC       3' RASMUS
+    3' GCATATGTATCCGTGATATAGCTGGTGTGAC 5' THORFINN
+  */
   for (int i = 0; i < r; i++){
     // right 5' overhangs, Rasmus's DMG pattern is fully dependent on that of Thorfinn.
     if (thorfinn[L-i-1] == 'C' || thorfinn[L-i-1] == 'c'){
+      if (i == 0){C_total++;G_total++;}
       double u = mrand_pop(mr);
       if (u < delta_s){
         IsDeam = 1;
         thorfinn[L-i-1] = 'T';
         rasmus[L-i-1] = 'A';
+        if (i == 0){C_to_T_counter++;G_to_A_counter++;}
       }
     }
   }
@@ -159,43 +171,75 @@ int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, 
       }
   }
   
-  
   // Way 2 Complicated Way (should be a little bit faster)
   for (int i = l; i < L-r; i++){
 
+    //if(i == l){fprintf(stderr,"Lille l %d and pos nick %d\n",l,p_nick_n);}
+    //fprintf(stderr,"p_nick_n %d \t p_nick_m %d \t L %d \n",p_nick_n,p_nick_m,L);
+    
+    
     if (i<L-p_nick_n-1 && (rasmus[i] == 'C' || rasmus[i] == 'c')){
+      if (i == 0){C_total++;G_total++;}
       //left of nick on thorfinn strand we change thorfinn according to rasmus
+
+      /*
+        5' CGTATACATAGGCACTATATCGACCACACT 3'
+        3' GCATATGTA CCGTGATATAGCTGGTGTGA 5'
+                      |
+                      v
+        5' CGTATACATAGGCACTATATCGACCACACT 3'
+        3'           CCGTGATATAGCTGGTGTGA 5'                
+      */
+    
       double u = mrand_pop(mr);
       if (u < delta){
         IsDeam = 1;
         rasmus[i] = 'T';
         thorfinn[i] = 'A'; //Downstream nick one DMG pattern depends on the other strand
+        if (i == 0){C_to_T_counter++;G_to_A_counter++;}
       }
     }
     else if (i>p_nick_m && (thorfinn[i] == 'C' || thorfinn[i] == 'c')){
+      if (i == (L-1)){C_total++;G_total++;}
       // right side of rasmus nick we change rasmus according to thorfinn
+
+      /*++
+        5' CGTATACAT GGCACTATATCGACCACACT 3'
+        3' GCATATGTATCCGTGATATAGCTGGTGTGA 5'
+                      |
+                      v
+        5' CGTATACAT                       3'
+        3' GCATATGTATCCGTGATATAGCTGGTGTGA  5'                
+      */
+
       double u = mrand_pop(mr);
       if (u < delta){
         IsDeam = 1;
         rasmus[i] = 'A';
         thorfinn[i] = 'T'; //Downstream nick one DMG pattern depends on the other strand
+        if (i == (L-1)){C_to_T_counter++;G_to_A_counter++;}
       }
     }
 	
     // between the nick with rasmus showing DMG
     else if(i>=L-p_nick_n-1 && i<=p_nick_m && (rasmus[i] == 'C' || rasmus[i] == 'c')){
+      if (i == 0){C_total++;G_total++;}
       double u = mrand_pop(mr);
       if (u < delta){
         IsDeam = 1;
         rasmus[i] = 'T'; //Upstream both nicks, DMG patterns are independent
+        if (i == 0){C_to_T_counter++;}
       }
     }
     // between the nick with Thorfinn showing DMG
     else if(i>=L-p_nick_n-1 && i<=p_nick_m && (thorfinn[i] == 'C' || thorfinn[i] == 'c')){
+      if (i == (L-1)){C_total++;G_total++;}
+      //fprintf(stderr,"i value %d\n",i);
       double u = mrand_pop(mr);
       if (u < delta){
         IsDeam = 1;
         thorfinn[i] = 'T'; //Upstream both nicks, DMG patterns are independent
+        if (i == (L-1)){C_to_T_counter++;}
       }
     }
   }
@@ -230,32 +274,66 @@ int SimBriggsModel2(char *ori, int L, double nv, double lambda, double delta_s, 
 #ifdef __WITH_MAIN__
 //g++ Briggs2.cpp -D__WITH_MAIN__ mrand.o
 int main(){
+  int C_total = 0;int C_to_T_counter = 0;int C_to_T_counter_rev = 0;int C_total_rev=0;
+  int G_total = 0;int G_to_A_counter = 0;int G_to_A_counter_rev = 0;int G_total_rev=0;
+
   int maxfraglength = 100;
-  mrand_t *mr = mrand_alloc(2,777);
-  char original[1024];
-  char **results = new char *[4];
-  for(int i=0;i<4;i++){
-    results[i] = new char[1024];
-    memset(results[i],'\0',1024);
-  }
+  int seed = 300;
+  mrand_t *mr = mrand_alloc(2,seed);
 
-  while(1){
+  size_t reads = 1000000;
+  int modulovalue = 10;
+  size_t moduloread = reads/modulovalue;
 
-    int flen = mrand_pop_long(mr) % maxfraglength;
-    //fprintf(stderr,"FLEN IS %d\n",flen);
+  for (int i = 0; i < reads; i++){
+    if (i%moduloread == 0){
+      fprintf(stderr,"SEQUENCE %d \n",i);
+    }
+    
+    char original[1024];
+    char **results = new char *[4];
+    for(int i=0;i<4;i++){
+      results[i] = new char[1024];
+      memset(results[i],'\0',1024);
+    }
+
+    int flen; 
+    flen = mrand_pop_long(mr) % maxfraglength;//mrand_pop_long(mr) % maxfraglength;
+    if (flen < 30){
+      flen = 35;
+    }
+    
     for(int i=0;i<flen;i++)
       original[i] = bass[mrand_pop_long(mr) %4];
+    
+    //fprintf(stderr,"FLEN IS %d\n",flen);
+    //fprintf(stderr,"ori:    %s\n",original);
+    //fprintf(stderr,"strlen IS %d\n",strlen(original));
 
-    fprintf(stderr,"ori: %s\n",original);
-    SimBriggsModel2(original,flen,0.024,0.36,0.68,0.9,mr,results);
+    int strand = 0;
+    //if (original[0] == 'C'){C_total++;};
+    SimBriggsModel2(original,flen,0.024,0.36,0.68,0.0097,mr,results,strand,C_to_T_counter,G_to_A_counter,C_total,G_total);
 
-    fprintf(stderr,"ori: %s\n",original);
-    for(int i=0;i<4;i++)
-      fprintf(stderr,"res[%d]: %s\n",i,results[i]);
+    int pair = mrand_pop(mr)>0.5?0:1;
+    
+    /*if (pair == 0 && original[0] == 'C'){
+      C_total++;
+      if (results[pair][0] == 'T' || results[pair+2][0] == 'T'){
+        C_to_T_counter++;
+      }
+    }
+    if (pair == 1 && original[0] == 'G'){
+      G_total++;
+      if (results[pair][0] == 'A' || results[pair+2][0] == 'A'){
+        G_to_A_counter++;
+      }
+    }*/
 
-    break;
   }
-
+  double C_Deam = (double)C_to_T_counter/(double)C_total;
+  double G_Deam = (double)G_to_A_counter/(double)G_total;
+  fprintf(stderr,"deamination C>T freq %f and G>A freq %f\n",C_Deam,G_Deam);
+  //fprintf(stderr,"G total counter %d and G > A counter %d and deamination freq %f\n",G_total,G_to_A_counter,G_Deam);
   return 0;
 }
 
--- Briggs2.h
+++ Briggs2.h
@@ -22,6 +22,6 @@
  */
 
 
-int SimBriggsModel2(char *original, int L, double nv, double lambda, double delta_s, double delta,mrand_t *mr,char **results, int strandR1);
+int SimBriggsModel2(char *original, int L, double nv, double lambda, double delta_s, double delta,mrand_t *mr,char **results, int strandR1,int& C_to_T_counter,int& G_to_A_counter,int& C_to_T_counter_rev,int& G_to_A_counter_rev);
 
 #endif /* NGSNGSFUNC_H */
