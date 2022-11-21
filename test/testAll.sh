82cdc973f8b705490b7a8c2cd140f92435ffd78f
RAHenriksen <rah@hotmail.dk>
2022-11-21 18:05:22 +0100

    temporary test files

--- test/testAll.sh
+++ test/testAll.sh
@@ -8,6 +8,9 @@ A1=AGATCGGAAGAGCACACGTCTGAACTCCAGTCACCGATTCGATCTCGTATGCCGTCTTCTGCTTG
 A2=AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATTT
 MF=../Test_Examples/MisincorpFile.txt
 
+VCFDIR=../Test_Examples/VCF
+VCFIN=../Test_Examples/VCF/MTSub.fa
+
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "---------------------------------------- Testing '${PRG}' examples -----------------------------------------"
 echo "---------------------------------------------------------------------------------------------------------------"
@@ -15,7 +18,8 @@ echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "1) Testing Single-end, length file, reads, briggs, sampling threads, bam"
 echo "---------------------------------------------------------------------------------------------------------------"
-${PRG} -i ${IN} -r 100000 -t1 2 -s 1 -lf ${LF} -seq SE -b 0.024,0.36,0.68,0.0097 -q1 ${Q1} -f bam -o MycoBactBamSEOut
+${PRG} -i ${IN} -r 100000 -t 2 -s 1 -lf ${LF} -seq SE -m b7,0.024,0.36,0.68,0.0097 -q1 ${Q1} -f bam -o MycoBactBamSEOut
+
 samtools sort MycoBactBamSEOut.bam -o MycoBactBamSEOutSort.bam
 Unsort_line=$(samtools view MycoBactBamSEOut.bam|wc -l)
 Sort_line=$(samtools view MycoBactBamSEOutSort.bam|wc -l)
@@ -28,7 +32,7 @@ echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "2) Testing Paired-end, fixed length, coverage, no sequencing error, adapters, sampling threads, fq"
 echo "---------------------------------------------------------------------------------------------------------------"
-${PRG} -i ${IN} -c 3 -t1 2 -s 1 -l 100 -seq PE -ne -a1 ${A1} -a2 ${A2} -q1 ${Q1} -q2 ${Q2} -f fq -o MycoBactFqPEOut
+${PRG} -i ${IN} -c 3 -t 2 -s 1 -l 100 -seq PE -ne -a1 ${A1} -a2 ${A2} -q1 ${Q1} -q2 ${Q2} -f fq -o MycoBactFqPEOut
 sort MycoBactFqPEOut_R1.fq > MycoBactFqPEOut_R1_sort.fq
 sort MycoBactFqPEOut_R2.fq > MycoBactFqPEOut_R2_sort.fq
 rm MycoBactFqPEOut_R1.fq
@@ -41,18 +45,37 @@ echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "3) Testing Single-end, length distributions, reads, misincorporation file, fa"
 echo "---------------------------------------------------------------------------------------------------------------"
-${PRG} -i ${IN} -r 100000 -t1 1 -s 1 -ld Pois,78 -seq SE -mf ${MF} -f fa -o MycoBactFaSEOut
+${PRG} -i ${IN} -r 100000 -t 1 -s 1 -ld Pois,78 -seq SE -mf ${MF} -f fa -o MycoBactFaSEOut
 md5sum MycoBactFaSEOut.fa >> MycoBactTest.md5
-echo " "
-echo "---------------------------------------------------------------------------------------------------------------"
-echo "----------------------------------------- Additional '${PRG}' Test -----------------------------------------"
-echo "---------------------------------------------------------------------------------------------------------------"
+
 echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
 echo "4) Testing Single-end, fixed length, reads, sequencing error, adapters, poly-G tail"
 echo "---------------------------------------------------------------------------------------------------------------"
-${PRG} -i ${IN} -r 100000 -t1 1 -s 1 -l 70 -seq SE -q1 ${Q1} -a1 ${A1} -p G -f fq -o MycoBactFqSEPolyOut
+${PRG} -i ${IN} -r 100000 -t 1 -s 1 -l 70 -seq SE -q1 ${Q1} -a1 ${A1} -p G -f fq -o MycoBactFqSEPolyOut
 md5sum MycoBactFqSEPolyOut.fq >> MycoBactTest.md5
+
+echo " "
+echo "---------------------------------------------------------------------------------------------------------------"
+echo "5) Testing Single-end, fixed length greater than error profile, reads, cycle length"
+echo "---------------------------------------------------------------------------------------------------------------"
+${PRG} -i ${IN} -r 100000 -t 1 -s 1 -l 200 -seq SE -q1 ${Q1} -cl 90 -f fq -o MycoBactFqSEClOut
+
+Mean_read_length=$(awk '{if(NR%4==2) {count++; bases += length} } END{print bases/count}' MycoBactFqSEClOut.fq)
+if [ $Mean_read_length -ne 90 ]; then 
+echo "Warning the mean read length isn't in accorandance with the cycle length"; exit 1;
+fi
+md5sum MycoBactFqSEClOut.fq >> MycoBactTest.md5
+
+echo "---------------------------------------------------------------------------------------------------------------"
+echo "6) Testing Single-end, simulating genetic variations from Haploid vcf files, for three indivdiuals"
+echo "---------------------------------------------------------------------------------------------------------------"
+
+for file in $(ls ${VCFDIR}/*Haploid*); do for indiv in {0,1,2}; do 
+    echo ${file} indiv ${indiv}; 
+    Type=$(ls ${file}| sed 's/.*Sub//'|sed 's/Haploid.vcf//');
+    ${PRG} -i ${VCFIN} -r 100 -t 1 -s 1 -l 150 -seq SE -ne -vcf ${file} -id ${indiv} -q1 ${Q1} -chr MT -—dump-internal VCF_temp_haploid_${Type}_${indiv} -f fq -o VCFtest;
+    md5sum VCF_temp_haploid_${Type}_${indiv}.fa >> VCF.md5; done; done
 
 echo " "
 echo "---------------------------------------------------------------------------------------------------------------"
@@ -63,3 +86,8 @@ echo " "
 #md5sum MycoBactFqPEOut_R1.fq >> MycoBactTest.md5
 #md5sum MycoBactFqPEOut_R2.fq >> MycoBactFQ.md5
 md5sum -c MycoBactTest.md5 || exit 2;
+
+
+md5sum -c VCF.md5 || exit 2;
+rm *.fq
+rm *.fa
